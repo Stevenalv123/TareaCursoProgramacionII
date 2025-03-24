8563860a25230eb96e9f208ef2ac75ad93cccd26
Stevenalv123 <stevenalv222@gmail.com>
2025-03-23 18:13:01 -0600

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ProyectoFinalProgramacionII
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TxtEmail.Focus();
+        }
+
+        private void TxtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TxtEmail.Text))
+            {
+                TextEmail.Visibility = Visibility.Collapsed; // Ocultar el placeholder
+            }
+            else
+            {
+                TextEmail.Visibility = Visibility.Visible; // Mostrar el placeholder si está vacío
+            }
+        }
+
+        private void TextPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TxtPassword.Focus();
+        }
+
+        private void TxtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TxtPassword.Password))
+            {
+                TextPassword.Visibility = Visibility.Collapsed; // Ocultar el placeholder
+            }
+            else
+            {
+                TextPassword.Visibility = Visibility.Visible; // Mostrar el placeholder si está vacío
+            }
+        }
+
+        private void TxtSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TxtEmail.Text) && !string.IsNullOrEmpty(TxtPassword.Password))
+            {
+                MessageBox.Show("Succesfully Signed In");
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
